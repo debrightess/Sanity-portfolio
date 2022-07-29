92eb241098175d4298c4c1725f83f9ac962a3cac
Bright Tenalo <debrightess@yahoo.com>
2022-07-29 12:29:32 +0100

    updated header

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import './Header.scss'
+// import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
 
-const Header = () => {
-  return <div>Header</div>
-}
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
 
-export default Header
+const Header = () => (
+  <div className='app__header app__flex'>
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className='app__header-info'
+    >
+      <div className='app__header-badge'>
+        <div className='badge-cmp app__flex'>
+          <div class='head-text' style={{ marginLeft: 30 }}>
+            <p className='p-text f-text'>
+              Hello, I am <span>👋</span>
+            </p>
+            <h1 className='head-text'>Bright</h1>
+
+            <p className='p-text sub'>I'm a Front End Developer</p>
+            <p className='p-text sub'>ReactJs developer and avid learner!</p>
+          </div>
+          <motion.img
+            whileInView={{ scale: [0, 1] }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            src={images.circle}
+            alt='profile_circle'
+            className='overlay_circle'
+          />
+        </div>
+      </div>
+    </motion.div>
+
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className='app__header-circles'
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className='circle-cmp app__flex' key={`circle-${index}`}>
+          <img src={circle} alt='profile_bg' />
+        </div>
+      ))}
+    </motion.div>
+  </div>
+);
+
+export default Header;
